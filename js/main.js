fe5435f1cac23d8f5a075d401032bbcbbe1f4529
Dmitriy <dima.96631@gmail.com>
2022-02-18 22:34:14 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".main-slider", {
+  slidePerview: 2,
+  spaceBetween: 30,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".main-slider__arrow",
+  },
+});
